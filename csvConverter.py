fea387ebc6118af265e624bbae3528026ceb94ca
Beddds <fathoniubay@gmail.com>
2023-04-26 16:36:15 +0700

    update converter

--- csvConverter.py
+++ csvConverter.py
@@ -1,5 +1,22 @@
 # convert csv
 
+# konverter bahan_bangunan.csv menjadi array
+def bahan(data): # data adalah sebuah string
+    arrBahan = [None for i in range(15)]
+    tempStr = ""
+    count = 0
+    for i in range(len(data)):
+        if data[i] == ";" or data[i] == "\n":
+            arrBahan[count] = tempStr 
+            tempStr = ""     
+            count += 1
+        else:
+            tempStr += data[i]
+    
+    # print(arrBahan)
+    # ['nama','deskripsi','jumlah',None,None, ...]
+    return arrBahan
+
 # konverter candi.csv menjadi array
 def candi(data): # data adalah sebuah string
     arrCandi = [None for i in range(505)]
@@ -33,23 +50,6 @@ def user(data): # data adalah sebuah string
     # print(arrUser)
     # ['username','password','role','Bondowoso','cintaroro','bandung_bondowoso', ...]
     return arrUser
-                
-# konverter bahan_bangunan.csv menjadi array
-def bahan(data): # data adalah sebuah string
-    arrBahan = [None for i in range(15)]
-    tempStr = ""
-    count = 0
-    for i in range(len(data)):
-        if data[i] == ";" or data[i] == "\n":
-            arrBahan[count] = tempStr 
-            tempStr = ""     
-            count += 1
-        else:
-            tempStr += data[i]
-    
-    # print(arrBahan)
-    # ['nama','deskripsi','jumlah',None,None, ...]
-    return arrBahan
 
 def konvert(readCandi,readUser,readBahan):
     # di read
