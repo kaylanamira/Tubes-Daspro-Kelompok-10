9b1e9ea2e523f129bc4a4d32d84c40f5c5c92646
Beddds <fathoniubay@gmail.com>
2023-04-30 01:54:09 +0700

    update udah bisa di run

--- F13.py
+++ F13.py
@@ -5,9 +5,29 @@ def load(namaFolder):
 
     # cek ada tidaknya namaFolder yang diberikan
     if os.path.exists(namaFolder) == False:
-        print(f'\nFolder "{namaFolder}" tidak ditemukan.')
-        # jika tidak ada maka keluar program
-        exit()
+        
+        if os.path.exists(os.path.join("save", namaFolder)) == False:
+            print(f'\nFolder "{namaFolder}" tidak ditemukan.')
+            # jika tidak ada keluar dari program
+            exit()
+        
+        else:
+            csvFileUser  = os.path.join("save", namaFolder, "user.csv")
+            csvFileCandi = os.path.join("save", namaFolder, "candi.csv")
+            csvFileBahan = os.path.join("save", namaFolder, "bahan_bangunan.csv")
+            
+            csvUser  = open(csvFileUser, 'r')
+            csvCandi = open(csvFileCandi, 'r')
+            csvBahan = open(csvFileBahan, 'r')
+            
+            tempVar = konvert(csvCandi,csvUser,csvBahan)
+            
+            # UI ditemukan folder
+            print("\nLoading...\n")
+            print('Selamat datang di program "Manajerial Candi"')
+            print("Silahkan masukkan username Anda\n")
+            
+            return tempVar
         
     else:
         csvFileUser  = os.path.join(namaFolder, "user.csv")
