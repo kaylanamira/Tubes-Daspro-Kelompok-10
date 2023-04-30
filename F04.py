b942fc83b34060acb5b0eb1b8360f379e10848b1
kaylanamira <kaylanmr04@gmail.com>
2023-04-30 12:44:16 +0700

    bug fixes

--- F04.py
+++ F04.py
@@ -1,30 +1,42 @@
 from custom_function import *
 
+def isAdaJin(data_user):
+    #menentukan apakah jin ada pada array data_user
+    jumlah_jin = hitungJin("jin_pengumpul",data_user) + hitungJin("jin_pembangun",data_user)
+    if (jumlah_jin == 0):
+        return False
+    else: #jumlah_jin > 0
+        return True
+
 def hapusjin(data_user,data_candi):
-    uname_jin = input("Masukkan username jin : ")
-    if not(adaUsername(uname_jin,data_user)):
-        print("\nTidak ada jin dengan username tersebut.\n")
+    #menghapus jin dari array data_user serta candi yang dibangun jin tersebut dari data_candi
+    if isAdaJin(data_user):
         uname_jin = input("Masukkan username jin : ")
+        while not(adaUsername(uname_jin,data_user)):
+            print("\nTidak ada jin dengan username tersebut.\n")
+            uname_jin = input("Masukkan username jin : ")
+        else:
+            yakin = input(f"Apakah anda yakin ingin menghapus jin dengan username {uname_jin} (Y/N)?")
+            if yakin == "N" or yakin == "n":
+                print("Jin tidak jadi dihapus.")
+            elif yakin == "Y" or yakin == "y":
+                #menghapus informasi jin dari array data_user
+                for i in range(0,360,3):
+                    if data_user[i] == uname_jin:
+                        #menghapus semua data pada 3 kolom yg berisi informasi jin
+                        for j in range(3):
+                            data_user[i+j] = None
+
+                #menghapus candi yang dibangun jin dari array data_candi
+                for m in range(1,505,5):
+                    if data_candi[m] == uname_jin:
+                        #menghapus semua data pada 5 kolom yg berisi informasi candi yang dibangun oleh jin terhapus
+                        for n in range(5):
+                            data_candi[m+n] = None
+
+                print("Jin telah berhasil dihapus dari alam gaib.")
     else:
-        yakin = input(f"Apakah anda yakin ingin menghapus jin dengan username {uname_jin} (Y/N)?")
-        if yakin == "N" or yakin == "n":
-            print("Jin tidak jadi dihapus.")
-        elif yakin == "Y" or yakin == "y":
-            #menghapus informasi jin dari array data_user
-            for i in range(0,360,3):
-                if data_user[i] == uname_jin:
-                    #menghapus semua data pada 3 kolom yg berisi informasi jin
-                    for j in range(3):
-                        data_user[i+j] = None
-
-            #menghapus candi yang dibangun jin dari array data_candi
-            for m in range(1,505,5):
-                if data_candi[m] == uname_jin:
-                    #menghapus semua data pada 5 kolom yg berisi informasi candi yang dibangun oleh jin terhapus
-                    for n in range(5):
-                        data_candi[m+n] = None
-
-            print("Jin telah berhasil dihapus dari alam gaib.")
+        print("Tidak ada jin untuk dihapus")
 
     return (data_user,data_candi)
 
--- F05.py
+++ F05.py
@@ -1,26 +1,31 @@
 from custom_function import *
+from F04 import *
 
 def ubahjin(data_user):
-    uname_jin = input("Masukkan username jin : ")
-
-    if not(adaUsername(uname_jin,data_user)):
-        print("\nTidak ada jin dengan username tersebut.\n")
+    #mengubah role jin
+    if isAdaJin(data_user):
         uname_jin = input("Masukkan username jin : ")
-    else:
-        idx_jin = get_index(uname_jin,data_user)
-        role = data_user[idx_jin + 2]
-        if role == "jin_pengumpul":
-            cek = input("Jin ini bertipe “Pengumpul”. Yakin ingin mengubah ke tipe “Pembangun” (Y/N)? ")
-            role = "jin_pembangun"
-        else:
-            cek = input("Jin ini bertipe “Pembangun”. Yakin ingin mengubah ke tipe “Pengumpul” (Y/N)? ")
-            role = "jin_pengumpul"
 
-        if cek == "Y":
-            data_user[idx_jin + 2] = role
-            print("\nJin telah berhasil diubah.\n")
+        while not(adaUsername(uname_jin,data_user)):
+            print("\nTidak ada jin dengan username tersebut.\n")
+            uname_jin = input("Masukkan username jin : ")
         else:
-            print("\nJin tidak jadi diubah.\n")
+            idx_jin = get_index(uname_jin,data_user)
+            role = data_user[idx_jin + 2]
+            if role == "jin_pengumpul":
+                cek = input("Jin ini bertipe “Pengumpul”. Yakin ingin mengubah ke tipe “Pembangun” (Y/N)? ")
+                role = "jin_pembangun"
+            else:
+                cek = input("Jin ini bertipe “Pembangun”. Yakin ingin mengubah ke tipe “Pengumpul” (Y/N)? ")
+                role = "jin_pengumpul"
+
+            if cek == "Y":
+                data_user[idx_jin + 2] = role
+                print("\nJin telah berhasil diubah.\n")
+            else:
+                print("\nJin tidak jadi diubah.\n")
+    else:
+        print("Tidak ada jin untuk diubah.")
 
     return data_user
 
